de0d816ad239a924b2c1f32f5db5fb4073bfd317
7204-OMER <Eyupbhr24@gmail.com>
2021-08-24 17:20:12 +0300

    add student list

--- usecontext/src/components/StudentItem.js
+++ usecontext/src/components/StudentItem.js
@@ -1,4 +1,8 @@
+import { useContext } from 'react'
+import { StudentContext } from '../context/StudentContext'
+
 const StudentItem = ({ student }) => {
+  const { changeColor }= useContext(StudentContext);  
   return (
     <div style={{ background: student.color, paddingBottom: '2rem', paddingTop: '1rem', marginBottom: '1rem' }}>
       <h3>{student.name}</h3>
